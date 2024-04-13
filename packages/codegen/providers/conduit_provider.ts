e81f15528572e9aba12fdf6741358f55e160ef7a
Julien Ripouteau <julien@ripouteau.com>
2024-04-13 15:08:53 +0200

    chore: wip

--- packages/codegen/providers/conduit_provider.ts
+++ packages/codegen/providers/conduit_provider.ts
@@ -1,16 +1,5 @@
-import { Route } from '@adonisjs/core/http'
 import type { ApplicationService } from '@adonisjs/core/types'
-import { LazyImport } from '@adonisjs/core/types/http'
-import { Constructor } from 'ts-morph'
 
-declare module '@adonisjs/core/http' {
-  export interface Router {
-    action: <T extends Constructor<any>>(
-      pattern: string,
-      handler: LazyImport<Constructor<any>>
-    ) => Route<T>
-  }
-}
 export default class TuyauProvider {
   constructor(protected app: ApplicationService) {}
 
