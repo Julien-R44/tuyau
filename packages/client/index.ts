df55e535fb9f5d719b042f471923980a203b5729
Julien Ripouteau <julien@ripouteau.com>
2024-04-28 18:54:27 +0200

    fix: unwrap export

--- packages/client/index.ts
+++ packages/client/index.ts
@@ -1,3 +1,3 @@
 export * from './src/types.js'
-export { createTuyau } from './src/client.js'
 export { TuyauHTTPError } from './src/errors.js'
+export { createTuyau, unwrap } from './src/client.js'
