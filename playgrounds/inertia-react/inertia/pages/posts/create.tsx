46151eb7c7bcfb12905488e23f81c0cb19a2d666
Urata Daiki <7nohe@users.noreply.github.com>
2025-02-19 10:00:32 +0900

    feat: add useRouter hook for manual route visits in React and Vue

--- playgrounds/inertia-react/inertia/pages/posts/create.tsx
+++ playgrounds/inertia-react/inertia/pages/posts/create.tsx
@@ -1,12 +1,22 @@
 import { Head } from '@inertiajs/react'
-import { Link } from '@tuyau/inertia/react'
+import { Link, useRouter } from '@tuyau/inertia/react'
 
 export default function Home() {
+  const router = useRouter()
+
   return (
     <>
       <Head title="Create post" />
-
       <div className="container">
+        <button
+          onClick={() =>
+            router.visit({
+              route: 'home',
+            })
+          }
+        >
+          Go to home
+        </button>
         <p>Create post</p>
         <Link route="home"></Link>
       </div>
